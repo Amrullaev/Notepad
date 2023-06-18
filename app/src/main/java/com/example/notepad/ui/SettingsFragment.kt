5bbde9989cd31e8291ebf7a8172223c1b47dcfd4
Amrullaev <amrullayevuchqun@gmail.com>
2023-06-18 23:13:17 +0500

    add Night mode

--- app/src/main/java/com/example/notepad/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/notepad/ui/SettingsFragment.kt
@@ -1,19 +1,26 @@
 package com.example.notepad.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.notepad.InfoFragment
 import com.example.notepad.R
 import com.example.notepad.databinding.FragmentFirstBinding
 import com.example.notepad.databinding.FragmentSettingsBinding
+import com.example.notepad.utils.SharedPref
+import com.example.notepad.utils.YourPreference
 
 
 class SettingsFragment : Fragment() {
     private lateinit var _binding: FragmentSettingsBinding
     private val mBinding get() = _binding
+    lateinit var yourPreference: YourPreference
+    private lateinit var sPref: SharedPreferences
 
 
     override fun onCreateView(
@@ -21,13 +28,32 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
-
+        yourPreference = YourPreference.getInstance(mBinding.root.context)
         mBinding.info.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.main_container, InfoFragment(), InfoFragment::javaClass.name)
                 .addToBackStack(InfoFragment::javaClass.name).commit()
         }
 
+
+        sPref = requireContext().getSharedPreferences("shared", Context.MODE_PRIVATE)
+        if (sPref.getString("mode", "")=="night"){
+            mBinding.themeSwitch.isChecked = true
+            mBinding.modeTv.text = "Tungi rejim"
+        }
+        mBinding.themeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (buttonView.isPressed) {
+                if (isChecked) {
+                    sPref.edit().putString("mode", "night").apply()
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    mBinding.modeTv.text = "Tungi rejim"
+                } else {
+                    sPref.edit().putString("mode", "day").apply()
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    mBinding.modeTv.text = "Kunduzgi rejim"
+                }
+            }
+        }
         return mBinding.root
     }
 
---
+++ app/src/main/java/com/example/notepad/ui/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.notepad.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.notepad.MainActivity
+import com.example.notepad.databinding.ActivitySplashBinding
+import com.example.notepad.utils.YourPreference
+import java.util.concurrent.Executors
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
